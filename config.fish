f1788654c19e09d78027debd439affb14c959654
ktakayama <kazuyuki.takayama@wealthnavi.com>
2018-08-01 14:16:39 +0900

    Change config.fish

--- config.fish
+++ config.fish
@@ -22,7 +22,7 @@ set __fish_git_prompt_color_upstream_behind red
 
 # Status Chars
 set __fish_git_prompt_char_dirtystate '⚡'
-set __fish_git_prompt_char_stagedstate '→'
+set __fish_git_prompt_char_stagedstate '->'
 set __fish_git_prompt_char_untrackedfiles '☡'
 set __fish_git_prompt_char_stashstate '↩'
 set __fish_git_prompt_char_upstream_ahead '+'
