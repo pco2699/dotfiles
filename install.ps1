e5734e0db23f69021bae828cadeaeaac8b9f5f4b
pco2699 <aenkun@gmail.com>
2022-11-30 16:20:56 -0500

    Create install.ps1

---
+++ install.ps1
@@ -0,0 +1,11 @@
+wsl --install
+
+winget install -e --id JetBrains.Toolbox
+winget install -e --id Microsoft.VisualStudioCode
+winget install -e --id Microsoft.WindowsTerminal
+winget install -e --id Microsoft.BingWallpaper
+winget install -e --id Google.Chrome
+winget install -e --id Discord.Discord
+winget install -e --id Google.Drive
+winget install -e --id Zoom.Zoom
+winget install -e --id Microsoft.PowerToys
