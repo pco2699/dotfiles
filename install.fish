1c3990425205af1bae434430b5412dea7eae275b
pco2699 <aenkun@gmail.com>
2023-02-18 18:12:08 -0500

    Create install.fish

---
+++ install.fish
@@ -0,0 +1,10 @@
+curl -sL https://git.io/fisher | source
+ln -sf ~/.dotfiles/fish_plugins ~/.config/fish
+fisher update
+
+mkdir -p ~/.config/fish/functions
+ln -sf ~/.dotfiles/config.fish ~/.config/fish
+
+ln -sf ~/.dotfiles/peco_select_history.fish ~/.config/fish/functions
+ln -sf ~/.dotfiles/peco_select_repository.fish ~/.config/fish/functions
+ln -sf ~/.dotfiles/fish_user_key_bindings.fish ~/.config/fish/functions
