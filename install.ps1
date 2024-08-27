e5b4ffa6431b700668db2cde69da0cc37cdeafb3
pco2699 <aenkun@gmail.com>
2024-08-26 23:00:21 -0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,16 +1,11 @@
-winget install -e --id JetBrains.Toolbox
 winget install -e --id Microsoft.VisualStudioCode
 winget install -e --id Microsoft.WindowsTerminal
 winget install -e --id Microsoft.BingWallpaper
-winget install -e --id Google.Chrome
 winget install -e --id Discord.Discord
 winget install -e --id Google.Drive
-winget install -e --id Zoom.Zoom
 winget install -e --id Microsoft.PowerToys
 winget install -e --id Git.Git
 winget install -e --id Foxit.FoxitReader
 winget install -e --id Amazon.Kindle
-winget install -e --id SlackTechnologies.Slack
-winget install -e --id Logitech.OptionsPlus
 winget install -e --id RandyRants.SharpKeys
 winget install -e DevToys
