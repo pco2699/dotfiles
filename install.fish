29e82181c11cb35e9c19c0afbeb209f26ec0549b
pco2699 <aenkun@gmail.com>
2024-09-08 16:54:02 -0700

    Update install.fish

--- install.fish
+++ install.fish
@@ -7,6 +7,4 @@ fisher update
 mkdir -p ~/.config/fish/functions
 ln -sf ~/.dotfiles/config.fish ~/.config/fish
 
-ln -sf ~/.dotfiles/peco_select_history.fish ~/.config/fish/functions
-ln -sf ~/.dotfiles/peco_select_repository.fish ~/.config/fish/functions
 ln -sf ~/.dotfiles/fish_user_key_bindings.fish ~/.config/fish/functions
